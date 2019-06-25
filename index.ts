fc1a706e0297a950ac10c21dd67358c74e066bf2
lastshadowpl <xeloxpl@windowslive.com>
2019-06-25 10:47:29 +0200

    Add settings

--- index.ts
+++ index.ts
@@ -2,19 +2,34 @@ let last = {
   x: 0,
   y: 0,
 }
-let start = 0
+
 const ball = document.querySelector('.ball') as HTMLElement
-const tran = 0.2
-const threshold = window.screen.height
-const diff = 10
-const changeMod = 1/3
+let tran = 0.15
+let th = 2.5
+const threshold = window.screen.height / th
+const diff = 100
+const changeMod = 1/2
 const startThreshold = 100
 
-ball.style.setProperty('--t', String(tran + 's'))
-
-document.addEventListener('touchstart', e => {
-  start = e.touches[0].clientY
+const tranInput = document.getElementById("tran") as HTMLElement
+tranInput.addEventListener("change", e => {
+  if(e.currentTarget) {
+    tran = +(e.currentTarget as HTMLInputElement).value || tran
+    ball.style.setProperty("--t", tran+"s")
+  } 
 })
+// @ts-ignore
+tranInput.value = tran;
+
+const thInput = document.getElementById("threshold") as HTMLElement
+tranInput.addEventListener("change", e => {
+  if(e.currentTarget) th = +(e.currentTarget as HTMLInputElement).value || th 
+})
+
+// @ts-ignore
+thInput.value = th;
+
+ball.style.setProperty('--t', String(tran + 's'))
 
 document.body.addEventListener('touchmove', e => {
   if(window.scrollY <= startThreshold) {
@@ -29,7 +44,7 @@ document.addEventListener('touchend', () => {
   ball.classList.add('return')
   const v = +ball.style.getPropertyValue('--y')
   ball.style.setProperty('--y', '0')
-  // console.clear()
+  console.log(v, threshold - diff)
   if(v >= threshold - diff) {
     console.log("You did it!")
   }
--- js/index.js
+++ js/index.js
@@ -3,17 +3,30 @@ let last = {
     x: 0,
     y: 0,
 };
-let start = 0;
 const ball = document.querySelector('.ball');
-const tran = 0.2;
-const threshold = window.screen.height;
-const diff = 10;
-const changeMod = 1 / 3;
+let tran = 0.15;
+let th = 2.5;
+const threshold = window.screen.height / th;
+const diff = 100;
+const changeMod = 1 / 2;
 const startThreshold = 100;
-ball.style.setProperty('--t', String(tran + 's'));
-document.addEventListener('touchstart', e => {
-    start = e.touches[0].clientY;
+const tranInput = document.getElementById("tran");
+tranInput.addEventListener("change", e => {
+    if (e.currentTarget) {
+        tran = +e.currentTarget.value || tran;
+        ball.style.setProperty("--t", tran + "s");
+    }
 });
+// @ts-ignore
+tranInput.value = tran;
+const thInput = document.getElementById("threshold");
+tranInput.addEventListener("change", e => {
+    if (e.currentTarget)
+        th = +e.currentTarget.value || th;
+});
+// @ts-ignore
+thInput.value = th;
+ball.style.setProperty('--t', String(tran + 's'));
 document.body.addEventListener('touchmove', e => {
     if (window.scrollY <= startThreshold) {
         ball.style.setProperty('--y', String(Math.min(e.touches[0].clientY * changeMod, threshold)));
@@ -23,7 +36,7 @@ document.addEventListener('touchend', () => {
     ball.classList.add('return');
     const v = +ball.style.getPropertyValue('--y');
     ball.style.setProperty('--y', '0');
-    // console.clear()
+    console.log(v, threshold - diff);
     if (v >= threshold - diff) {
         console.log("You did it!");
     }
